3c456840c4b1b3d245aa8b373d714b731f4a714c
zhangjianghao <jianghaozhang@qq.com>
2022-06-28 21:43:34 +0800

    reformat

--- chapter2/find_duplicate_number.cpp
+++ chapter2/find_duplicate_number.cpp
@@ -6,11 +6,12 @@ using namespace std;
  * find any ONE duplicate number in array
  * */
 bool find(int numbers[], int length, int* duplication){
-	if (numbers == nullptr || length == 0){
+	if (numbers == nullptr || length == 0)
+	{
 		return false;
 	}
 
-	for (int i = 0; i < length; ++i)
+	for (int i = 0; i < length; ++i)	
 	{
 		if (numbers[i] <0 || numbers[i] > length-1)
 		{
@@ -20,7 +21,7 @@ bool find(int numbers[], int length, int* duplication){
 	}
 
 
-	for (int i = 0; i < length; ++i)
+	for (int i = 0; i < length; ++i)	
 	{
 		while(numbers[i] != i){
 			if (numbers[i] == numbers[numbers[i]])
@@ -38,7 +39,7 @@ bool find(int numbers[], int length, int* duplication){
 
 			int temp = numbers[i];
 			numbers[i] = numbers[temp];
-            numbers[temp] = temp;
+			numbers[temp] = temp;
 		}
 	}
 	return false;
@@ -47,7 +48,7 @@ bool find(int numbers[], int length, int* duplication){
 int main(){
 	int numbers[] = {2,3,1,0,2,5,3};
 	bool result = find(numbers, 7, &numbers[0]);
-	if (result == true)
+	if (result == true)	
 	{
 		cout << "Really find dup" << endl;
 	}
